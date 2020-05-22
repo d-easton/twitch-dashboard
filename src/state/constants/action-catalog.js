af845383c177991c74c50918112eb757e7d5d85e
David <d-easton@wustl.edu>
2020-05-22 18:02:15 -0400

    added basic redux actions (testing git connection)

---
+++ src/state/constants/action-catalog.js
@@ -0,0 +1,8 @@
+/*
+    Catalog of available actions, listed as constants to avoid typos and string errors
+*/
+
+export const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN";
+
+export const PAGE_FORWARD = "PAGE_FORWARD";
+export const PAGE_BACKWARD = "PAGE_BACKWARD";
